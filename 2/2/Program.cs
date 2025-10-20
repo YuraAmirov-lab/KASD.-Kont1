21b3b2af645435c6da62f0b9c180174456641293
Юрий <amirovurij@gmail.com>
2025-10-20 20:06:39 +0300

    Add files via upload

---
+++ 2/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] input = Console.ReadLine().Split(' ');
+        int[] count = new int[101];
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(input[i]);
+            count[number]++;
+        }
+        for (int number = 0; number <= 100; number++)
+        {
+            for (int j = 0; j < count[number]; j++)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
