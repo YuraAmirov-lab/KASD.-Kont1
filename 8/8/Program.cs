e2208d2ff6b8c20522a18b0b3c94030d29867642
Юрий <amirovurij@gmail.com>
2025-11-17 19:25:24 +0300

    Add files via upload

---
+++ 8/8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double C = double.Parse(Console.ReadLine());
+
+        double left = 0.0;
+        double right = 1000000.0; 
+
+        for (int i = 0; i < 10000; i++)
+        {
+            double mid = (left + right) / 2.0;
+            double value = mid * mid + Math.Sqrt(mid);
+
+            if (value < C)
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        Console.WriteLine(left.ToString("F6"));
+    }
+}
