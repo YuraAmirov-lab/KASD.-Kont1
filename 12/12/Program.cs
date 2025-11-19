cd747c9c052b66f489554be290e070a19d2128d5
Юрий <amirovurij@gmail.com>
2025-11-19 19:48:39 +0300

    Add files via upload

---
+++ 12/12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        long n = long.Parse(input[0]);
+        long k = long.Parse(input[1]);
+
+        long left = 1;
+        long right = n * n;
+
+        while (left < right)
+        {
+            long mid = left + (right - left) / 2;
+            long count = 0;
+
+            for (long i = 1; i <= n; i++)
+            {
+                count += Math.Min(n, mid / i);
+            }
+
+            if (count >= k)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        Console.WriteLine(left);
+    }
+}
---
+++ 13/13/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        long k = long.Parse(input[1]);
+
+        string firstL = Console.ReadLine();
+        string[] first = firstL.Split();
+        long[] a = new long[first.Length];
+        for (int i = 0; i < first.Length; i++)
+        {
+            a[i] = long.Parse(first[i]);
+        }
+
+        string secondL = Console.ReadLine();
+        string[] second = secondL.Split();
+        long[] b = new long[second.Length];
+        for (int i = 0; i < second.Length; i++)
+        {
+            b[i] = long.Parse(second[i]);
+        }
+
+        Array.Sort(a);
+        Array.Sort(b);
+
+        long left = a[0] + b[0];
+        long right = a[n - 1] + b[n - 1];
+
+        while (left < right)
+        {
+            long mid = left + (right - left) / 2;
+            long count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                long target = mid - a[i];
+                count += Count(b, target);
+            }
+
+            if (count >= k)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        Console.WriteLine(left);
+    }
+
+    static long Count(long[] arr, long target)
+    {
+        int left = 0;
+        int right = arr.Length;
+
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (arr[mid] <= target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
